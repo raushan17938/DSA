f3ed59208a1de408e7359ae92e40303711d55258
Raushan Kumar Sharma <146665460+raushan17938@users.noreply.github.com>
2025-01-17 23:43:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    vector<int> productExceptSelf(vector<int>& arr) {
+        int n = arr.size();
+        
+        
+        vector<int> left(n, 1), right(n, 1), result(n);
+        
+   
+        for (int i = 1; i < n; i++) {
+            left[i] = left[i - 1] * arr[i - 1];
+        }
+        
+
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * arr[i + 1];
+        }
+        
+      
+        for (int i = 0; i < n; i++) {
+            result[i] = left[i] * right[i];
+        }
+        
+        return result;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution obj;
+        vector<int> res = obj.productExceptSelf(arr);
+
+        for (int i = 0; i < res.size(); i++) {
+            cout << res[i] << " ";
+        }
+        cout << endl;
+        cout << "~\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
