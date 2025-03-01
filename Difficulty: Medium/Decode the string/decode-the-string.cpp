664821e0a6d18621ce602bf83ab778a02a6e80b3
Raushan Kumar Sharma <146665460+raushan17938@users.noreply.github.com>
2025-03-01 23:50:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Decode the string/decode-the-string.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+ 
+public:
+    string decodedString(string &s) {
+        stack<string> str;
+        stack<int> num;
+        string cur = "", temp;
+        int n = 0;
+
+        for (char c : s) {
+            if (isdigit(c)) n = n * 10 + (c - '0');
+            else if (c == '[') { str.push(cur); num.push(n); cur = ""; n = 0; }
+            else if (c == ']') {
+                temp = cur;
+                cur = str.top(); str.pop();
+                for (int i = 0, x = num.top(); i < x; i++) cur += temp;
+                num.pop();
+            } else cur += c;
+        }
+        return cur;
+    }
+
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+
+        Solution ob;
+        cout << ob.decodedString(s) << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
