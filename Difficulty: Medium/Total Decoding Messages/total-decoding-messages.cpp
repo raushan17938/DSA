e1ac2fc845472c20937da44a43223ffce07c53ae
Raushan Kumar Sharma <146665460+raushan17938@users.noreply.github.com>
2025-03-23 23:15:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Total Decoding Messages/total-decoding-messages.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int countWays(string &s) {
+        if (s[0] == '0') return 0;
+        int n = s.size(), prev2 = 1, prev1 = 1;
+        for (int i = 1; i < n; i++) {
+            int curr = 0;
+            if (s[i] != '0') curr = prev1;
+            int num = (s[i - 1] - '0') * 10 + (s[i] - '0');
+            if (num >= 10 && num <= 26) curr += prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    cin.ignore();
+    while (tc--) {
+        string digits;
+        getline(cin, digits);
+        Solution obj;
+        int ans = obj.countWays(digits);
+        cout << ans << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
