10c6527ac04968fce9ec96146bb7ec0f2f92b608
Raushan Kumar Sharma <146665460+raushan17938@users.noreply.github.com>
2025-04-23 23:53:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Unique Number II/unique-number-ii.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+   public:
+    vector<int> singleNum(vector<int>& arr) {
+        int x = 0;
+        for (int n : arr) x ^= n;
+        int mask = x & -x;
+        int a = 0, b = 0;
+        for (int n : arr) {
+            if (n & mask) a ^= n;
+            else b ^= n;
+        }
+        return a < b ? vector<int>{a, b} : vector<int>{b, a};
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        vector<int> ans = ob.singleNum(arr);
+        for (auto it : ans) {
+            cout << it << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
