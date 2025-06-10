73bc1c5ab2d2327c54501bfa2fcc6d4d70aee5fd
Raushan Kumar Sharma <146665460+raushan17938@users.noreply.github.com>
2025-06-10 22:10:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
@@ -0,0 +1,20 @@
+// User function Template for C++
+class Solution {
+  public:
+    int nthFibonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int prev2 = 0; 
+        int prev1 = 1; 
+        int curr;
+
+        for (int i = 2; i <= n; ++i) {
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return curr;
+    }
+};
