aeead920babb7ecfb5cadbc8e3fc779b4d26ef55
Raushan Kumar Sharma <146665460+raushan17938@users.noreply.github.com>
2025-06-11 23:12:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Reverse a String/reverse-a-string.cpp
+++ Difficulty: Basic/Reverse a String/reverse-a-string.cpp
@@ -1,46 +1,14 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 // User function Template for C++
 class Solution {
   public:
-    string reverseString(string& s) {
-        // code here
-        int n = s.length()-1;
-        int i = 0;
-        while(i<=n){
-            swap(s[i++], s[n--]);
-            
+    string revStr(string s) {
+     
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            swap(s[left], s[right]);
+            left++;
+            right--;
         }
-        
         return s;
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    cin >> t;
-    while (t--) {
-        string s;
-        cin >> s;
-        Solution ob;
-        cout << ob.reverseString(s) << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends
