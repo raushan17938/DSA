5a98cadada9d0530f64e9944669e731a1adaf30a
Raushan Kumar Sharma <146665460+raushan17938@users.noreply.github.com>
2025-06-17 23:32:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Map Operations/map-operations.cpp
@@ -0,0 +1,25 @@
+map<int, int> mapInsert(vector<int>& arr) {
+    map<int, int> mp;
+   
+    for (int i = 0; i < arr.size(); ++i) {
+        mp[arr[i]] = i; 
+    }
+    return mp;
+}
+
+void mapDisplay(map<int, int>& mp) {
+  
+    for (auto& pair : mp) {
+        cout << pair.first << " " << pair.second << endl;
+    }
+}
+
+void mapErase(map<int, int>& mp, int x) {
+ 
+    if (mp.erase(x)) {
+        cout << "erased " << x << endl;
+    } else {
+      
+        cout << "not found" << endl;
+    }
+}
