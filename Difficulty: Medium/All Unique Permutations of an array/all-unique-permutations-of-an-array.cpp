a9ca569e88a3ea85b9722021b737360a8d390bce
Raushan Kumar Sharma <146665460+raushan17938@users.noreply.github.com>
2025-10-01 23:42:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/All Unique Permutations of an array/all-unique-permutations-of-an-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<vector<int>> uniquePerms(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        vector<vector<int>> res;
+        do {
+            res.push_back(arr);
+        } while (next_permutation(arr.begin(), arr.end()));
+        return res;
+    }
+};
