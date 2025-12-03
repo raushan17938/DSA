86a0e5552640fc346ac0ad9773748dccd73e1311
Raushan Kumar Sharma <146665460+raushan17938@users.noreply.github.com>
2025-12-03 19:46:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.cpp
+++ Difficulty: Easy/Second Largest/second-largest.cpp
@@ -3,16 +3,18 @@ class Solution {
     int getSecondLargest(vector<int> &arr) {
         // code here
         int n = arr.size();
-        
         int largest = arr[0];
-        int slargest = -1;
         
-        for(int i =1; i<n; i++){
+        for(int i =1; i< n; i++){
             if(arr[i] > largest){
-                slargest = largest;
                 largest = arr[i];
             }
-            else if(arr[i]!= largest && arr[i] > slargest){
+        } 
+        
+        int slargest = INT_MIN;
+        
+        for(int i=0; i<n; i++){
+            if(arr[i] > slargest && arr[i] != largest){
                 slargest = arr[i];
             }
         }
