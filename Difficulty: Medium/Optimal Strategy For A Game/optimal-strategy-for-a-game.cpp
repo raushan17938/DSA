812e55db1c31aa5ad78fd4f3103f864076438bf0
Raushan Kumar Sharma <146665460+raushan17938@users.noreply.github.com>
2025-12-06 21:50:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Optimal Strategy For A Game/optimal-strategy-for-a-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int maximumAmount(vector<int> &arr) {
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(n));
+
+        for (int gap = 0; gap < n; ++gap) {
+            for (int i = 0, j = gap; j < n; ++i, ++j) {
+                int x = ((i + 2) <= j) ? dp[i + 2][j] : 0;
+                int y = ((i + 1) <= (j - 1)) ? dp[i + 1][j - 1] : 0;
+                int z = (i <= (j - 2)) ? dp[i][j - 2] : 0;
+
+                dp[i][j] = max(arr[i] + min(x, y), arr[j] + min(y, z));
+            }
+        }
+        return dp[0][n - 1];
+    }
+};
