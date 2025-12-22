58365752feb917a3e7613265d20afdbf560be308
Raushan Kumar Sharma <146665460+raushan17938@users.noreply.github.com>
2025-12-22 14:21:31 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.81%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,23 +3,22 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int> temp;
+        int j = -1;
 
         for(int i=0; i<n; i++){
-
-            if(nums[i] != 0){
-                temp.push_back(nums[i]);
+            if(nums[i] == 0) {
+                j=i;
+                break;
             }
         }
 
-        int nz = temp.size();
+        if(j == -1) return;
 
-        for(int i=0; i<nz; i++){
-            nums[i] = temp[i];
-        }
-
-        for(int i = nz; i<n; i++){
-            nums[i] = 0;
+        for(int i=j+1; i<n; i++){
+            if(nums[i] !=0){
+                swap(nums[i], nums[j]);
+                j++;
+            }
         }
     }
 };
