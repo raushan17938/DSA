eaeebccc4f16f8bae7ef821a0cf17e949fc41c1f
Raushan Kumar Sharma <146665460+raushan17938@users.noreply.github.com>
2025-12-22 13:31:54 +0530

    Time: 0 ms (100%), Space: 24.7 MB (5.35%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int nonZero =0; //hamari non-zero value 0 index par aani chahiye
-        for(int j =0; j<nums.size(); j++){
-            if(nums[j] != 0){
-                swap(nums[j], nums[nonZero]);
-                nonZero++;
+        int n = nums.size();
+
+        vector<int> temp;
+
+        for(int i=0; i<n; i++){
+
+            if(nums[i] != 0){
+                temp.push_back(nums[i]);
             }
+        }
+
+        int nz = temp.size();
+
+        for(int i=0; i<nz; i++){
+            nums[i] = temp[i];
+        }
+
+        for(int i = nz; i<n; i++){
+            nums[i] = 0;
         }
     }
 };
